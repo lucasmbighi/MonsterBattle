825be32684e4471e6471476444d30be66a7e686f
Lucas Bighi <lucasmbighi@icloud.com>
2024-10-22 13:52:31 -0300

    Initial Commit

---
+++ MonsterBattle/MonsterBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterBattleApp.swift
+//  MonsterBattle
+//
+//  Created by Lucas Bighi on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MonsterBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
